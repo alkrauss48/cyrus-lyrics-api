47bc9b4313fa78efccc1ee5d818143944000cb36
aaron <alkrauss48@gmail.com>
2022-01-27 14:50:40 -0600

    Marshal google oauth config from environment Instead of credentials.json file

--- helpers.go
+++ helpers.go
@@ -1,8 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"os"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -11,13 +12,48 @@ import (
 	"google.golang.org/api/drive/v3"
 )
 
-func getGoogleOAuthConfig() (*oauth2.Config, error) {
-	b, err := ioutil.ReadFile("credentials.json")
-	if err != nil {
-		return nil, fmt.Errorf("Unable to read client secret file: %v", err)
+type GoogleConfig struct {
+	Web SubGoogleConfig `json:"web"`
+}
+
+type SubGoogleConfig struct {
+	ClientId                string    `json:"client_id"`
+	ProjectId               string    `json:"project_id"`
+	AuthUri                 string    `json:"auth_uri"`
+	TokenUri                string    `json:"token_uri"`
+	AuthProviderX509CertUrl string    `json:"auth_provider_x509_cert_url"`
+	ClientSecret            string    `json:"client_secret"`
+	RedirectUris            [1]string `json:"redirect_uris"`
+}
+
+func marshalOAuthCredentials() ([]byte, error) {
+	credentials := GoogleConfig{
+		Web: SubGoogleConfig{
+			AuthProviderX509CertUrl: os.Getenv("AUTH_PROVIDER_CERT_URL"),
+			AuthUri:                 os.Getenv("AUTH_URI"),
+			ClientId:                os.Getenv("CLIENT_ID"),
+			ClientSecret:            os.Getenv("CLIENT_SECRET"),
+			ProjectId:               os.Getenv("PROJECT_ID"),
+			RedirectUris:            [1]string{os.Getenv("REDIRECT_URI")},
+			TokenUri:                os.Getenv("TOKEN_URI"),
+		},
 	}
 
-	config, err := google.ConfigFromJSON(b, drive.DriveFileScope)
+	jsonCredentials, err := json.Marshal(credentials)
+	if err != nil {
+		return nil, err
+	}
+
+	return jsonCredentials, nil
+}
+
+func getGoogleOAuthConfig() (*oauth2.Config, error) {
+	jsonCredentials, err := marshalOAuthCredentials()
+	if err != nil {
+		return nil, err
+	}
+
+	config, err := google.ConfigFromJSON(jsonCredentials, drive.DriveFileScope)
 	if err != nil {
 		return nil, fmt.Errorf("Unable to parse client secret file to config: %v", err)
 	}
