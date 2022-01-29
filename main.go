07989661063ce8148e9fc620c159f824e3a01acd
aaron <alkrauss48@gmail.com>
2022-01-29 10:58:45 -0600

    Add root route

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
+	router.GET("/", root)
 	router.GET("/oauth/google", googleLogin)
 	router.GET("/oauth/google/callback", googleLoginCallback)
 	router.GET("/oauth/google/processed", googleLoginProcessed)
@@ -15,4 +18,10 @@ func main() {
 	router.GET("/sheets/:id", getSheetByID)
 
 	router.Run(":8000")
+}
+
+func root(c *gin.Context) {
+	c.String(http.StatusOK, "You've found the CyrusLyrics API. "+
+		"For more information over the CyrusLyrics iOS app, navigate "+
+		"to https://cyruskrauss.com")
 }
