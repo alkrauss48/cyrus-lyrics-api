3733f6d6951121eb7aeea9fc60a14b9f3df00d3f
aaron <alkrauss48@gmail.com>
2022-04-18 11:52:15 -0500

    1.0.3 test

--- public/root.go
+++ public/root.go
@@ -7,7 +7,7 @@ import (
 )
 
 func Root(c *gin.Context) {
-	c.String(http.StatusOK, "TEST 2: You've found the CyrusLyrics API. "+
+	c.String(http.StatusOK, "TEST 1.0.3: You've found the CyrusLyrics API. "+
 		"For more information over the CyrusLyrics iOS app, navigate "+
 		"to https://cyruskrauss.com")
 }
