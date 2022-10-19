fd1508f4371871e6f70631a5b7431f181fd2ab23
aaron <alkrauss48@gmail.com>
2022-10-18 21:44:49 -0500

    Revert "Add Innotech 2022 root page content"
    This reverts commit ec599f438f7ab4f944a98352587c15e2b398937a.

--- public/root.go
+++ public/root.go
@@ -7,6 +7,7 @@ import (
 )
 
 func Root(c *gin.Context) {
-	c.String(http.StatusOK, "Hello Innotech! To register for this talk's course, "+
-		"navigate to https://clvr.sh/innotech2022")
+	c.String(http.StatusOK, "You've found the CyrusLyrics API. "+
+		"For more information over the CyrusLyrics iOS app, navigate "+
+		"to https://cyruskrauss.com")
 }
